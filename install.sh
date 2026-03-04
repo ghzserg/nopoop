908fd5081db1e44ee8eb9e6b9651ec9f20da861f
Namida Verasche <namida.verasche@gmail.com>
2026-03-04 18:57:10 +1300

    Removed filament.json altogether; removed most of the readme and replaced with a link to up-to-date info.

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-#[ -f /opt/config/mod_data/filament.json ] && cp /opt/config/mod_data/filament.json /opt/config/mod_data/filament.json.save && echo "Old file filament.json saved us filament.json.save"
-#cp filament.json /opt/config/mod_data/filament.json && echo "New file filament.json installed"
 echo "SAVE_ZMOD_DATA use_trash_on_print=0" >/tmp/printer
 echo "Plugin Nopoop installed"
+echo "For usage info please see: https://wiki.zmod.link/AD5X/#slicer-controlled-purge"
 echo "DISPLAY_OFF" >/tmp/printer
